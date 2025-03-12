0ac75c791b5917e7fa8ef81db06211dccf21cd7c
ashreesee <ashreechaurasia43@gmail.com>
2025-03-12 16:17:52 +0530

    544

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,7 +5,7 @@ import react from "@vitejs/plugin-react-swc";
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: '0.0.0.0',  // Allows external access
+    host: '0.0.0.0',
     port: 5000,
     strictPort: true
   }
